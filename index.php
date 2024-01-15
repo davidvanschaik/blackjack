1ab66155f737cffd282b411598244e55daa79a7d
David van Schaik <davidvan.schaik00@gmail.com>
2024-01-15 12:23:54 +0100

    cleaned up index

--- index.php
+++ index.php
@@ -8,52 +8,79 @@ require_once("./Card.php");
 require_once("./Hand.php");
 
 $dealer = new Dealer(new Blackjack(), new Deck(6));
-$name = ['Jennifer', 'Foivos', 'Cesco', 'Nando', 'Nique', 'Romano', 'Milo', 'Gaby', 'Eugene', 'Jose', 'Sarah',
-'Luuk', 'Kian', 'Stefan', 'Iris', 'KlangKuenstler', 'Kobosil', 'Oguz', 'Joris'];
-
 
 while (true) {
-    $AutoPlay = readline("Do you want to play with friends or autoplay? (friends - auto) => .. ");
-    if ($AutoPlay == 'auto' || $AutoPlay == 'friends') {
-        $amount = readline("Please enter how many players you would like to play with? (2 - 12) => .. ");
+    $autoPlay = readline("Do you want to play with friends or autoplay? (friends - auto) => .. ");
 
-        if (!is_numeric($amount) || $amount < 2 || $amount > 17) {
-            echo "Invalid amount of players, try again" . PHP_EOL;
-            die;
-        } else {
-            switch ($AutoPlay) {
-                case 'friends':
+    if ($autoPlay == 'friends' || $autoPlay == 'auto') {
+        $amountPlayers = getAmountPlayers();
 
-                    for ($x = 0; $x < $amount ; $x++) { 
-                        while(true) {
-                            $name = readline("What's your name? => ..  ");
-
-                            if (!preg_match('/^[a-z]*$/i', $name) || empty($name)) {
-                                echo 'Invalid name, please try again. ' . PHP_EOL;
-                            } else {
-                                $bet = readline($name . ', please place your bet: (5 - 500) => .. ');
-
-                                if (!is_numeric($bet) || $bet < 5 || $bet > 500 || empty($bet)) {
-                                    echo 'Invalid bet, please try again.' . PHP_EOL;
-                                } else {
-                                    break;
-                                }
-                            }
-                        }
-                        $dealer->addPlayers(new Player($name, $bet));
-                    }
-                case 'auto':
-                    shuffle($name);
-
-                    for ($x = 0; $x < $amount; $x++) {
-                        $dealer->addPlayers(new Player(array_pop($name), random_int(5, 500)));
-                    }
-                }
-            }
+        switch ($autoPlay) {
+            case 'auto':
+                setPlayersAutomatically($amountPlayers, $dealer);
+                break;
+            case 'friends':
+                setPlayersManually($amountPlayers, $dealer);
+                break;
+        } 
         $dealer->addPlayers(new Player('Dealer', 0));
         echo PHP_EOL . "Let's Play!";
         $dealer->dealCards();
         $dealer->playGame();
+    } else {
+        echo 'Invalid input, please try again.' . PHP_EOL;
+    }
+}
+
+function getAmountPlayers(): int
+{
+    $amount = readline('How many players would you like to play with? (2 - 12) .. ');
+
+    if (!is_numeric($amount) || $amount < 2 || $amount > 12) {
+        echo 'Amount of players invalid, please try again.' . PHP_EOL;
+    } else {
+        return $amount;
+    }
+}
+
+function setPlayersAutomatically($amount, $dealer): void
+{
+    $name = ['Jennifer', 'Foivos', 'Cesco', 'Nando', 'Nique', 'Romano', 'Milo', 'Gaby', 'Eugene', 'Jose', 'Sarah',
+    'Luuk', 'Kian', 'Stefan', 'Iris', 'KlangKuenstler', 'Kobosil', 'Oguz', 'Joris'];
+
+    for ($x = 0; $x < $amount; $x++) {
+        $dealer->addPlayers(new Player(array_pop($name), random_int(5, 500)));
+    }
+}
+
+function setPlayersManually($amount, $dealer): void
+{
+    for ($x = 0; $x < $amount; $x++) { 
+        $name = getName();
+        $bet = getBet();
+        $dealer->addPlayers(new Player($name, $bet));
+    }
+}
+
+function getName(): string
+{
+    $name = readline("What's your name? => ..  ");
+
+    if (!preg_match('/^[a-z]*$/i', $name)) {
+        echo 'Invalid name, please try again.' . PHP_EOL;
+    } else {
+        return $name;
+    }
+}
+
+function getBet(): string
+{
+    $bet = readline('Please enter your bet. ');
+
+    if (!is_numeric($bet) || $bet < 5 || $bet > 500) {
+        echo 'Invalid bet, please try again.' . PHP_EOL;
+    } else {
+        return $bet;
     }
 }
 
